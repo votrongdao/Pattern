33ab8337a138d52acc662ff7d5f8d584fa30ddbe
LeeFrost <sera_dante@mail.ru>
2013-10-01 15:33:17 +0300

    Singleton realization in simple type

---
+++ Singelton/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singelton
+{
+    /// <summary>
+    /// Singleton realizaiotn in simple type
+    /// For using with theard need realise MethodImpl attribute or use lock() 
+    /// </summary>
+    public class Singleton
+    {
+        private static Singleton _instance;
+
+        private Singleton(){}
+
+        public static Singleton GetSingleton
+        {
+            get { return _instance ?? (_instance = new Singleton()); }
+        }
+    }
+}
