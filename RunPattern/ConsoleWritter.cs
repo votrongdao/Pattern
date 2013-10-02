3d7cd669b8e6b8b2a9a187dc72ec7b0be3d667d5
LeeFrost <sera_dante@mail.ru>
2013-10-02 21:53:09 +0300

    Some my feature - now we can use different output writers Only need realize interface IOutputWritter.cs

---
+++ RunPattern/ConsoleWritter.cs
@@ -0,0 +1,26 @@
+﻿// File: ConsoleWritter.cs
+// Coder: Sergiy Lischuk
+// Created: 02:10:2013  20:16
+// Email: evertodante@gmail.com
+
+#region
+
+using System;
+
+#endregion
+
+namespace RunPattern
+{
+    public class ConsoleWritter : IOutputWritter
+    {
+        public void WriteLine(string output)
+        {
+            Console.WriteLine(output);
+        }
+
+        public void WriteLine(string output, object[] parameter)
+        {
+            Console.WriteLine(output, parameter);
+        }
+    }
+}
---
+++ RunPattern/IOutputWritter.cs
@@ -0,0 +1,13 @@
+﻿// File: IOutputWritter.cs
+// Coder: Sergiy Lischuk
+// Created: 02:10:2013  20:07
+// Email: evertodante@gmail.com
+
+namespace RunPattern
+{
+    public interface IOutputWritter
+    {
+        void WriteLine(string output);
+        void WriteLine(string output, object[] parameter);
+    }
+}
