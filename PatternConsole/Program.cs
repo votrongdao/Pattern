a5cde045c0b96f45c22a279e62b3df75db325875
Leefrost <sera_dante@mail.ru>
2013-10-05 23:02:04 +0400

    Add IOutpputWritter realization

--- PatternConsole/Program.cs
+++ PatternConsole/Program.cs
@@ -7,14 +7,14 @@ namespace PatternConsole
         static void Main(string[] args)
         {
             Run runner;
-            // = new SomeBasicElemnts.WeaponRunner();
+            runner = new SomeBasicElemnts.WeaponRunner();
             // = new Observer.RunObserver();
             // = new Decorator.RunDecorator();
             // = new FactoryMethod.RunFactory();
             //runner = new Command.RunCommand();
             //runner = new Adapter.RunAdapter();
 //            runner = new Facade.RunFacade();
-            runner = new TemplateMethod.RunTemplateMethod();
+            //runner = new TemplateMethod.RunTemplateMethod();
             runner.RunApp();
         }
     }
--- SomeBasicElemnts/Bone.cs
+++ SomeBasicElemnts/Bone.cs
@@ -1,11 +1,12 @@
 ﻿
+using RunPattern;
 using SomeBasicElemnts.Interfaces;
 
 namespace SomeBasicElemnts
 {
     public class Bone:Weapon
     {
-        public Bone(string name,int dmg):base(dmg, name)
+        public Bone(string name,int dmg, IOutputWritter writter):base(dmg, name, writter)
         {
             Type = new BoneType();
 
